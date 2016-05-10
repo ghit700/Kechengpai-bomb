213970e84ea14a5e51b4d4949f54cb7879106aac
ghit700 <332668280@qq.com>
2016-05-10 18:59:45 +0800

    beat3

--- app/src/main/java/com/ketangpai/constant/Constant.java
+++ app/src/main/java/com/ketangpai/constant/Constant.java
@@ -2,6 +2,8 @@ package com.ketangpai.constant;
 
 import android.os.Environment;
 
+import com.ketangpai.utils.AppContextUtils;
+
 import java.io.File;
 
 /**
@@ -14,6 +16,8 @@ public class Constant {
     public final static String DATA_FOLDER = "data" + File.separator;
     public static final String ALBUM_PATH = Environment.getExternalStorageDirectory() + File.separator + MAIN_FOLDER + File.separator;
     public final static String PHOTO_FOLDER = ALBUM_PATH + "photo" + File.separator;
-    public final static String LOGO_FOLDER = PHOTO_FOLDER + "logo.jpg";
+    public final static String LOGO_FOLDER = PHOTO_FOLDER
+            + AppContextUtils.getInstance().getSharedPreferences("user", 0).getString("account", "")
+            + "logo.jpg";
 
 }
